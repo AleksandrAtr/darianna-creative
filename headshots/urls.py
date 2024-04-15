11f5763ecc0480b518fc85831a39c463ec3b1564
AleksandrAtr <atraleks@gmail.com>
2024-04-15 20:37:17 +0100

    refact: Use includes for products display in templates

--- headshots/urls.py
+++ headshots/urls.py
@@ -4,6 +4,6 @@ from products.views import product_detail
 
 # Define URL patterns for the headshots app
 urlpatterns = [
-    path('headshots', views.photo_studio_products, name='headshots'),
+    path('', views.photo_studio_products, name='headshots'),
     path('product_detail/<product_id>/', product_detail, name='product_detail'),
 ]
