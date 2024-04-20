7286b4b2d58bd7500a8289528923ec5c2914e9dd
AleksandrAtr <atraleks@gmail.com>
2024-04-20 18:22:45 +0100

    add: Update Review view, url, template for basic Add review functionality

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # Define URL patterns for the home app
 urlpatterns = [
-    path('', views.add_review, name='add_review')
+    path('add_review/<int:product_id>', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
+from .forms import ReviewForm
 
 # Create your views here.
-def add_review(request):
-    return render(request, 'reviews/add_review.html')
+def add_review(request, product_id):
+    
+    product = get_object_or_404(Product, pk=product_id)
+    
+    form = ReviewForm()
+    
+    template = 'reviews/add_review.html'
+
+    context = {
+        'form': form,
+        'product': product,
+    }
+    print(context)
+
+    return render(request, template, context)
+
