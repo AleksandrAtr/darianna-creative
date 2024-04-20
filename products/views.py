41385a76b1c48a0c2dae7b59b3f0dccb99dab35b
AleksandrAtr <atraleks@gmail.com>
2024-04-20 13:31:37 +0100

    feat: Product Admin secure views to restrict non-authorised activity

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from products.models import Product, Category
+from products.models import Product
+from django.contrib.auth.decorators import login_required
 from .forms import ProductForm
 from django.contrib import messages
+
 
 # Create your views here.
 def product_detail(request, product_id):
@@ -28,11 +30,17 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-     # Render the product detail page with the product data
+    # Render the product detail page with the product data
     return render(request, 'products/product_detail.html', context)
 
+
+@login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -52,8 +60,17 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -76,8 +93,13 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
