20e09b1ca8c0e428f1a4d79ed0c6225331ff77dc
AleksandrAtr <atraleks@gmail.com>
2024-04-20 13:17:35 +0100

    feat: Admin edit/delete product functionality

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('product-detail/<product_id>/', views.product_detail, name='product-detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
