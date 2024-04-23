b8e8abda34fdcba5f80f0ee1991cbc132927d22b
AleksandrAtr <atraleks@gmail.com>
2024-04-23 07:29:46 +0100

    fix: Workshops category, add favicon, add static_root

--- darianna_creative/settings.py
+++ darianna_creative/settings.py
@@ -182,6 +182,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Define the STATIC_ROOT setting
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
